dfffd95e4e77a8b10b74c8a986eeb5ebd82ba32c
yuriy6996 <megaigra76@gmail.com>
2026-06-03 20:18:44 +0300

    feat: реализованы чтение файлов и запись результатов

---
+++ analyzer.cpp
@@ -0,0 +1,43 @@
+#include "analyzer.h"
+
+QStringList readCodeFromFile(QFile& inputFile, QSet<ErrorMessage>& errorSet) {
+    QStringList codeLines;
+    QTextStream stream(&inputFile);
+    int currentLine = 0; // Нумерация строк с 0
+
+    while (!stream.atEnd()) {
+        QString line = stream.readLine();
+
+        if (currentLine >= 10000) {
+            errorSet.insert(ErrorMessage(LineLimitExceeded, currentLine, 0));
+            break;
+        }
+
+        if (line.length() > 1024) {
+            errorSet.insert(ErrorMessage(CharLimitExceeded, currentLine, 0));
+            break;
+        }
+
+        codeLines.append(line);
+        currentLine++;
+    }
+
+    return codeLines;
+}
+
+void writeResultsToFile(QFile& outputFile, const QSet<ErrorMessage>& errorSet) {
+    QTextStream out(&outputFile);
+
+    if (errorSet.isEmpty()) {
+        out << "Ошибок не обнаружено\n";
+        return;
+    }
+
+    QString outputBuffer = "Обнаружены ошибки:\n";
+
+    for (const ErrorMessage& error : errorSet) {
+        outputBuffer += error.generateText() + "\n";
+    }
+
+    out << outputBuffer;
+}
